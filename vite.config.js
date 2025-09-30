f07c29e19f6f313b80c97decf6045f0a1a28b20e
FreddieYK <f2662610@gmail.com>
2025-09-30 16:55:40 +0800

    fix: Add root level config files for Vercel deployment

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: './frontend',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          antd: ['antd']
+        }
+      }
+    }
+  },
+  base: '/'
+})
